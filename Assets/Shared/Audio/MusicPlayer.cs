1a4d9434f62fab669a446320e415d66736b2d8f1
Hamerite <45439796+Hamerite@users.noreply.github.com>
2020-02-12 22:57:02 -0500

    Audio/Buttons/Astetics
    Audio/BG/font added, buttons polished

---
+++ Assets/Shared/Audio/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioClip[] clips;
+
+    AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = true;
+        audioSource.loop = false;
+    }
+
+    void Update()
+    {
+        if(!audioSource.isPlaying)
+        {
+            audioSource.clip = clips[Random.Range(0, clips.Length)];
+            audioSource.Play();
+        }
+    }
+}
